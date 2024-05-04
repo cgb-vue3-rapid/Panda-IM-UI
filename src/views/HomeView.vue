d8f8db22895c793f3d31ca8c3354e3a9b92305fa
QiuTian-324 <1240092443@qq.com>
2024-05-04 22:47:20 +0800

    :tada:init:初始化项目

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,6 +4,9 @@ import TheWelcome from '../components/TheWelcome.vue'
 
 <template>
   <main>
+    <h1 class="text-3xl font-bold underline text-blue-600">
+      Hello world!
+    </h1>
     <TheWelcome />
   </main>
 </template>
